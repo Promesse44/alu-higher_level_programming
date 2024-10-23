bd0315a11c722520be5533416e2891ca609b7863
Promesse44 <p.irakoze2@alustudent.com>
2024-10-23 09:07:21 -0700

    alu

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+a = 10
+b = 5
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    add = add(a, b)
+    sub = sub(a, b)
+    mul = mul(a, b)
+    div = div(a, b)
+
+    output = "{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {}".format(a, b, add, a, b, sub, a, b, mul, a, b, div)
+
+    print(output)

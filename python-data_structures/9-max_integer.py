4e1befda3ff285e2b976c9d315f192a7b1f1eaeb
Promesse44 <p.irakoze2@alustudent.com>
2024-10-23 11:14:44 -0700

    alu

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    if sentence:
-        sentence_length = len(sentence)
-    else:
-        sentence_length = 0
-    return tuple((sentence_length, None if not sentence else sentence[:1]))
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max = my_list[0]
+    for item in my_list:
+        if item > max:
+            max = item
+    return max

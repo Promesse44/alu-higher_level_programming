39c77ab9478fab90410cdf9a93b3cbe2823005a3
Promesse44 <p.irakoze2@alustudent.com>
2024-11-15 07:23:41 -0800

    alu

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- a script that creates the database and table on MYSQL server
+CREATE  DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL);
